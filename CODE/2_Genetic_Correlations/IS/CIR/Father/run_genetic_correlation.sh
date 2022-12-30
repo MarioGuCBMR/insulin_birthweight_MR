197d2eabc2dae99e654dbf17e6d48615896d974b
MarioGuCBMR <64911610+MarioGuCBMR@users.noreply.github.com>
2022-12-30 11:28:51 +0100

    Add files via upload

---
+++ CODE/2_Genetic_Correlations/IS/CIR/Father/run_genetic_correlation.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#$ -S /bin/bash
+#$ -N "CIR_FATHER_GC"
+#$ -cwd
+#$ -pe smp 3
+#$ -l mem_free=4G
+#$ -l h_vmem=7G
+#$ -l s_vmem=4G
+
+conda activate r_env
+
+Rscript /emc/cbmr/users/zlc436/HERMINA_PhD4_Project/CODE/2_Genetic_Correlations/0_installing_HDL_and_reference_data/HDL_lax/HDL.run.R \
+gwas1.df=/emc/cbmr/users/zlc436/HERMINA_PhD4_Project/CODE/2_Genetic_Correlations/curated_files_4_gc/CIR_4_HDL.txt \
+gwas2.df=/emc/cbmr/users/zlc436/HERMINA_PhD4_Project/CODE/2_Genetic_Correlations/curated_files_4_gc/Father_4_HDL.txt \
+LD.path=/emc/cbmr/users/zlc436/HERMINA_PhD4_Project/CODE/2_Genetic_Correlations/0_installing_HDL_and_reference_data/ref_panel/UKB_imputed_hapmap2_SVD_eigen99_extraction \
+output.file=/emc/cbmr/users/zlc436/HERMINA_PhD4_Project/CODE/2_Genetic_Correlations/output/CIR_Father.Rout
